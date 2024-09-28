58b3e33261de8303e522b001e22f6a5a262511b1
KirShet <kirshet2000@gmail.com>
2024-09-28 23:37:55 +0600

    Implement method to fetch followers in github.service.ts
    Added a getFollowers method to the GithubService for retrieving a list of followers from the GitHub API.
    Utilized HttpClient to send a GET request to the GitHub API endpoint http://api.github.com/users/{userName}/followers.
    The method returns an Observable that can be subscribed to for handling the fetched data in

--- src/app/github.service.ts
+++ src/app/github.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GithubService {
 
-  constructor() { }
+  private apiUrl = 'http://api.github.com/users/';
+
+  constructor(private http: HttpClient) { }
+
+  getFollowers(userName: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}${userName}/followers`);
+  }
 }
