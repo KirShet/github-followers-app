c053f9afc558930d92996fcb18ab1ab95812b766
KirShet <kirshet2000@gmail.com>
2024-09-29 00:46:49 +0600

    add logging for followers loading
    Добавлен вывод в консоль при получении фолловеров в методе onFollowersLoaded.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ export class AppComponent {
   followers: any[] = [];
 
   onFollowersLoaded(followers: any[]) {
+    console.log("Followers received:", followers); 
     this.followers = followers;
   }
 }
